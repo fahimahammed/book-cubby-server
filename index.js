9006fb24af01c88acb658badf71c849a3fca8da5
fahimahammed <fahimahammed.cse@gmail.com>
2021-04-02 14:22:50 +0600

    updated

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zpqcv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 client.connect(err => {
   const collection = client.db("bookCubby").collection("books");
   const orderCollection = client.db("bookCubby").collection("orders");
